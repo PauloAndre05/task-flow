666eb76bad49c94227b626d51e955e030b564a63
Paulo André <pauloandrepaulinho05@gmail.com>
2025-07-26 16:37:49 +0100

    fix: Alteração na ordem dos botões do menu em sidebar

--- src/components/sidebar/Links.ts
+++ src/components/sidebar/Links.ts
@@ -3,8 +3,8 @@ import * as Icon from "../../assets/sidebar-Icons/index";
 
 export const DataLinks = [
   { id: 1, name: "Overview", path: "/", icon: Icon.Overview },
-  { id: 2, name: "Mentors", path: "/mentors", icon: Icon.Mentors },
-  { id: 3, name: "Message", path: "/message", icon: Icon.Message },
-  { id: 4, name: "Task", path: "/task", icon: Icon.Task },
+  { id: 2, name: "Task", path: "/task", icon: Icon.Task },
+  { id: 3, name: "Mentors", path: "/mentors", icon: Icon.Mentors },
+  { id: 4, name: "Message", path: "/message", icon: Icon.Message },
   { id: 5, name: "Setting", path: "/setting", icon: Icon.Setting },
 ];
