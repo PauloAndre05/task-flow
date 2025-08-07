1794ed48f03783ce45ff67dc749c89150d6472e7
Paulo André <pauloandrepaulinho05@gmail.com>
2025-08-07 16:48:27 +0100

    Criando a página de mentores

--- src/pages/mentors/Mentors.tsx
+++ src/pages/mentors/Mentors.tsx
@@ -1,8 +1,13 @@
 import { use, useEffect } from "react";
+import { RecentMentors } from "../../components/mentors/Recent-Mentors";
+import { MentorsList } from "../../components/mentors/Mentors-List";
 
 export const Mentors = () => {
   useEffect(() => {
     document.title = "Task Flow - Mentors";
   }, []);
-  return <h1>Mentors</h1>;
+  return <div className="flex flex-col gap-4 pl-6 pt-6 pb-6">
+    <RecentMentors />
+    <MentorsList />
+  </div>;
 };
