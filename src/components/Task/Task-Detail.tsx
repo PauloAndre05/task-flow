d326ec87555ed9ade5626a3ef6330ea410d6e4fe
Paulo André <pauloandrepaulinho05@gmail.com>
2025-08-19 18:01:06 +0100

    task-detail

--- src/components/Task/Task-Detail.tsx
+++ src/components/Task/Task-Detail.tsx
@@ -2,11 +2,11 @@ import { SubHeader } from "../subHeader/subHeader";
 import { ImageTaskToday } from "../../assets/overview";
 import { BsClock } from "react-icons/bs";
 import { PiUsers } from "react-icons/pi";
-import {IoCheckmarkCircleSharp} from "react-icons/io5"
+import { IoCheckmarkCircleSharp } from "react-icons/io5";
 
 const DataDetailTask = [
   { id: 1, description: "Understanding the tools in Figma" },
-  { id: 2, description: "Understand the basics of making designs", },
+  { id: 2, description: "Understand the basics of making designs" },
   { id: 3, description: "Design a mobile application with figma" },
 ];
 
@@ -70,16 +70,51 @@ export const TaskDetail = () => {
             <div className="flex flex-col gap-4">
               <h1 className="text-2xl font-semibold">Essence of Assessment</h1>
               <div className="flex flex-col gap-4">
-                {DataDetailTask.map(detail => (
+                {DataDetailTask.map((detail) => (
                   <div key={detail.id} className=" flex gap-3 items-center">
-                    <IoCheckmarkCircleSharp color="#546FFF" size={24}/> <span>{detail.description}</span>
+                    <IoCheckmarkCircleSharp color="#546FFF" size={24} />{" "}
+                    <span>{detail.description}</span>
                   </div>
                 ))}
               </div>
             </div>
           </div>
         </div>
-        <div className="border w-[23rem]"></div>
+        <div className="w-[30%] rounded-md p-6 bg-white h-fit">
+          <span className="font-semibold">Assigned Assignments</span>
+          <div className="my-6">
+            <h1 className="text-2xl font-semibold mb-3">
+              Creating Awesome Mobile Apps
+            </h1>
+            <span className="text-[#54577A]">UI UX Design . Apps Design</span>
+          </div>
+          <h2 className="text-2xl font-semibold">Detail Student</h2>
+          <div className="my-6 flex flex-col gap-4">
+            <div className="flex justify-between items-center">
+              <span className="text-[#54577A]">Student's name</span>
+              <span className="font-semibold">Skylar Dias</span>
+            </div>
+            <div className="flex justify-between items-center">
+              <span className="text-[#54577A]">Student Class</span>
+              <span className="font-semibold">MIPA 2</span>
+            </div>
+            <div className="flex justify-between items-center">
+              <span className="text-[#54577A]">Student Number</span>
+              <span className="font-semibold">10</span>
+            </div>
+          </div>
+          <h2 className="text-2xl font-semibold">File Task</h2>
+          <div className="felx justify-between items-center my-4">
+            <span className="text-[#54577A]">Last Modified</span>
+            <span>1 July 2022</span>
+          </div>
+          <span className="text-[#54577A]">File submissions</span>
+          <form className="mt-2">
+            <input type="file" name="" id="" className="w-full border-dashed border-2 border-[#546FFF] p-4 place-content-center rounded-md h-[15rem] outline-0"/>
+          </form>
+          <p className="text-sm my-2 text-[#54577A]"t>*drag or browser from device</p>
+          <button type="submit" className="w-full h-11 bg-[#546FFF] text-white rounded-xl cursor-pointer transition-colors hover: hover:bg-blue-50 hover:text-gray-700 mt-14">Submit</button>
+        </div>
       </div>
     </div>
   );
