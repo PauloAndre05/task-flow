b65405e32522a2379492517ef495fffd452b3996
Paulo André <pauloandrepaulinho05@gmail.com>
2025-11-12 21:34:01 +0000

    feat:

--- src/components/message/Chat.tsx
+++ src/components/message/Chat.tsx
@@ -1,13 +1,26 @@
 import { DataMentors } from "../mentors/DataMentors";
-import { videocall, voicecall, logoTaskFlow } from "../../assets/message/index";
+import {
+  videocall,
+  voicecall,
+  logoTaskFlow,
+  iconBottomSendMessage,
+  iconClipFile,
+} from "../../assets/message/index";
 import { PiLockSimple } from "react-icons/pi";
+import { useForm } from "react-hook-form";
+import { useState } from "react";
 
 interface ChatProps {
   user?: number;
 }
 
+type InputType = {
+  message?: string;
+};
+
 export const Chat = ({ user }: ChatProps) => {
   const userChat = DataMentors.find((u) => u.id === user);
+  const [message, setMessage] = useState<string>("");
 
   const getUserInitials = (name: string) => {
     const world = name.trim().split(/\s+/);
@@ -15,6 +28,13 @@ export const Chat = ({ user }: ChatProps) => {
     const lastInitial =
       world.length > 1 ? world[world.length - 1].charAt(0).toUpperCase() : "";
     return `${firstInitial}${lastInitial}`;
+  };
+
+  const { register, handleSubmit, reset } = useForm<InputType>();
+  const handleSendMessage = (data: InputType) => {
+    console.log("Message sent:", data.message);
+    setMessage(data.message || "");
+    reset();
   };
 
   return (
@@ -59,10 +79,35 @@ export const Chat = ({ user }: ChatProps) => {
               />
             </div>
           </div>
-          <div>Conversas</div>
-          <div className="bg-white stick b-0">ssad</div>
+          <div className=" p-8 flex-1 flex items-end justify-end">
+            {message && (
+              <div className="bg-[#546FFF] text-white p-2 rounded-lg text-sm max-w-[40%] text-wrap">
+                {message}
+              </div>
+            )}
+          </div>
+          <form
+            onSubmit={handleSubmit(handleSendMessage)}
+            className="bg-white mt-auto flex justify-between items-center py-3 px-8"
+          >
+            <textarea
+              placeholder="Send you message"
+              className="outline-none flex-1 mr-4 py-4 resize-none overflow-y-auto rounded-md text-sm text-gray-700 hidden-scrollbar"
+              rows={1}
+              {...register("message")}
+            />
+            <div className="flex items-center gap-4">
+              <button className="cursor-pointer">
+                <img src={iconClipFile} alt="Clip-File-Icon" />
+              </button>
+              <button className="cursor-pointer" type="submit">
+                <img src={iconBottomSendMessage} alt="Clip-File-Icon" />
+              </button>
+            </div>
+          </form>
         </div>
       ) : (
+        /* View if no user selected */
         <div className="flex-1 flex flex-col items-center justify-center  py-14">
           <div className="inset-0  flex-1 flex flex-col justify-center items-center gap-1">
             <img src={logoTaskFlow} alt="" className="w-24" />
